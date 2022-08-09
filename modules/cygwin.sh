15825b11bc2a992f2de6cff3a2db8149353b215b
HE Yaowen <he.yaowen@hotmail.com>
2022-08-09 10:28:26 +0800

    Add cygwin module: cygwin_apt-get

--- modules/cygwin.sh
+++ modules/cygwin.sh
@@ -5,3 +5,12 @@ module_cygwin_init() {
 
 	cp "$base_dir"/config/cygwin/.minttyrc "$HOME"
 }
+
+module_cygwin_apt-cyg() {
+    git clone https://github.com/transcode-open/apt-cyg.git /usr/local/apt-cyg
+
+    ln -s /usr/local/apt-cyg/apt-cyg /usr/local/bin
+
+    apt-cyg cache /var/cache/cygwin
+    apt-cyg mirror $cygwin_mirror_url
+}
