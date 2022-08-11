3739419b82647e29d53160a18a5822a086b6f633
HE Yaowen <he.yaowen@hotmail.com>
2022-08-11 11:11:42 +0800

    Add darwin module: darwin_pkgs

--- modules/darwin.sh
+++ modules/darwin.sh
@@ -1,3 +1,7 @@
 module_darwin_brew() {
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
+
+module_darwin_pkgs() {
+    brew install coreutils gnu-sed gnu-tar findutils jq wget zsh python nodejs php lua
+}
